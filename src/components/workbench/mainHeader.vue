bf256fef430a855e6e6107bbb54eeb8dc42db8a1
母东 <mudong@sefon.com>
2017-11-03 17:16:37 +0800

    ccc

--- src/components/workbench/mainHeader.vue
+++ src/components/workbench/mainHeader.vue
@@ -5,7 +5,7 @@
       <strong><i class="fa fa-clock-o"></i> 时间：</strong> <span v-text="`${datetime.getFullYear()}年${datetime.getMonth()}月${datetime.getDate()}日 ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds() < 10 ? '0' + datetime.getSeconds(): datetime.getSeconds()} 星期${week}`"></span>
       <small class="seg"></small>
       <span v-if="weather !== null">
-        <img :src="'https://cdn.heweather.com/cond_icon/' + weather.now.cond_code + '.png'" width="24" height="24">
+        <img :src="'/static/images/weather/' + weather.now.cond_code + '.png'" width="22" height="22">
         <strong>天气：</strong><span v-text="`${weather.basic.admin_area}-${weather.basic.location}    ${weather.now.cond_txt}    ${weather.now.wind_sc}   ${weather.now.tmp}°C`"></span>
       </span>
       <span v-else>
