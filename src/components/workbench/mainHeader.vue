ad9c2a7efd049b5dc7ee682f014c7b36b111bb63
母东 <mudong@sefon.com>
2017-11-07 08:44:58 +0800

    ddd

--- src/components/workbench/mainHeader.vue
+++ src/components/workbench/mainHeader.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="main-header clearfix">
-    <div class="col-sm-12 col-md-6"><strong>位置:</strong> </div>
+    <div class="col-xs-12">
+      <strong>位置:</strong>
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
   </div>
 </template>
 
